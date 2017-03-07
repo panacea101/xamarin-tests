df1f12019b1b19c8a22a32ec684a7b6045869d0f
panacea101 <panacea101@gmail.com>
2017-03-07 11:36:01 -0500

    Commit new changes

--- ReactNativeTests/ReactNativeTests/Pages/Accounts.cs
+++ ReactNativeTests/ReactNativeTests/Pages/Accounts.cs
@@ -1,10 +1,88 @@
 ﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.iOS;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
 namespace ReactNativeTests
 {
 	public class Accounts
 	{
+		protected IApp app;
+		//protected Platform platform;
+		protected Query homeIcon;
+		protected Query createWithEmail;
+		protected Query createWithFacebook;
+		protected Query createWithGoogle;
+		protected Query signInWithExistingAccount;
+		protected Query skipButton;
+		protected Query sothebysPermission;
+		protected Query orText;
+		//protected Query emailInput;
+		//protected Query passwordInput;
+
 		public Accounts()
 		{
+			createWithEmail = x => x.Marked("create with email button");
+			createWithFacebook = x => x.Marked("create with facebook button");
+			createWithGoogle = x => x.Marked("create with google button");
+			skipButton = x => x.Marked("skip button");
+			signInWithExistingAccount = x => x.Marked("sign in with existing sotheby's account button");
+			sothebysPermission = x => x.Marked("sothebys will never post to facebook without your permission text");
+			orText = x => x.Marked("or text");
+			app = AppInitializer.App;
+			//emailInput = x => x.Marked("email address textinput");
+			//passwordInput = x => x.Marked("password textinput");
 		}
+
+		public Accounts assertEverythingLoadedAccounts()
+		{
+			app.WaitForElement(createWithEmail);
+			app.WaitForElement(createWithFacebook);
+			app.WaitForElement(createWithGoogle);
+			app.WaitForElement(skipButton);
+			app.WaitForElement(sothebysPermission);
+			app.WaitForElement(orText);
+			app.Screenshot("account screen did load");
+			return this;
+		}
+
+		public void createWithEmailTap()
+		{
+			app.Tap(createWithEmail);
+		}
+
+		public Accounts createWithFacebookTap() // make void
+		{
+			app.Tap(createWithFacebook);
+			return this;
+
+		}
+
+		public Accounts createWithGoogleTap() // make void
+		{
+			app.Tap(createWithGoogle);
+			return this;
+		}
+
+		public Accounts signInWithExistingTap() // make void
+		{
+			app.Tap(signInWithExistingAccount);
+			return this;
+		}
+
+		//public Accounts enterEmailAddress()
+		//{
+		//	app.Tap(emailInput);
+		//	app.EnterText(emailInput, "RNTest1@sothebysqa.com");
+		//	app.Screenshot("email address textinput");
+		//	return this;
+		//}
+		//public Accounts enterPassword()
+		//{
+		//	app.Tap(passwordInput);
+		//	app.EnterText(passwordInput, "testing123");
+		//	return this;
+		//}
 	}
 }
--- ReactNativeTests/ReactNativeTests/Tests/AccountsTest.cs
+++ ReactNativeTests/ReactNativeTests/Tests/AccountsTest.cs
@@ -1,10 +1,70 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
 namespace ReactNativeTests
 {
+	[TestFixture(Platform.Android)]
+	[TestFixture(Platform.iOS)]
 	public class AccountsTest
 	{
-		public AccountsTest()
+		Platform platform;
+
+		public AccountsTest(Platform platform)
 		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			AppInitializer.App = AppInitializer.StartApp(platform);
+		}
+
+		[Test]
+		public void RunRepl()
+		{
+			AppInitializer.App.Repl();
+
+		}
+
+
+		//[Test]
+		//public void signInExistingAccount()
+		//{
+		//	new Accounts()
+		//		.signInWithExistingTap()
+		//		.enterEmailAddress()
+		//		.enterPassword();
+		//}
+
+	//	["continuousIntegration"]
+		[Test]
+		public void tapCreateWithFacebook()
+		{
+			new Accounts()
+				.assertEverythingLoadedAccounts()
+				.createWithEmailTap();
+		}
+
+	//	["continuousIntegration"]
+		[Test]
+		public void tapCreateWithGoogle()
+		{
+			new Accounts()
+				.assertEverythingLoadedAccounts()
+				.createWithGoogleTap();
+		}
+
+		[Test]
+		public void tapCreateWithEmail()
+		{
+			new Accounts()
+				.assertEverythingLoadedAccounts()
+				.createWithEmailTap();
 		}
 	}
 }
